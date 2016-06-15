52ff57d3c60d548a9a00766df6d62b7a1ff7f673
Chang Wen-Kai <kai0517@gmail.com>
2016-06-16 00:30:22 +0800

    Add VectorUtility.cpp for utility functions of Vector

--- Vector/Vector.cpp
+++ Vector/Vector.cpp
@@ -1,42 +1,8 @@
 #include "Vector.h"
-#include <iostream>
 #include <exception>
 #include <iterator>
 
-double read_and_sum(int size)
-{
-	Vector v(size);
-	double sum = 0;
-	try
-	{
-		for (int i = 0; i != v.size(); ++i)
-		{
-			std::cin >> v[i];
-		}
 
-		for (int i = 0; i != v.size(); ++i)
-		{
-			sum += v[i];
-		}
-	}
-	catch (std::out_of_range& e)
-	{
-		std::cout << e.what() << "\n";
-		return 0;
-	}
-
-	return sum;
-}
-
-void print_vector(Vector& v)
-{
-	std::cout << "The vector [";
-	for (int i = 0; i < v.size(); ++i)
-	{
-		std::cout << (i == 0 ? "" : ", ") << v[i];
-	}
-	std::cout << "]\n";
-}
 
 Vector::Vector(int size)
 	: m_pos{ 0 }
